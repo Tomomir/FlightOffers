f89512e55a6cf4a2724213ec9d8cae0d23021d2a
Tomas Pecuch <tomas.p14@gmail.com>
2020-03-26 16:09:56 +0100

    -init

---
+++ FlightOffers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightOffers
+//
+//  Created by Tomas Pecuch on 26/03/2020.
+//  Copyright © 2020 sk.pecuch.tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
